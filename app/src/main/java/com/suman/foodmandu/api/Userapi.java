6170db97aab62309601a682dfb215b585dda3b42
SsumanShahi <shaggysy201118@gmail.com>
2020-01-04 18:50:46 +0545

    signup api and code is coded for signup

--- app/src/main/java/com/suman/foodmandu/api/Userapi.java
+++ app/src/main/java/com/suman/foodmandu/api/Userapi.java
@@ -1,14 +1,24 @@
 package com.suman.foodmandu.api;
 
 import com.suman.foodmandu.model.User;
+import com.suman.foodmandu.serverresponse.ImageResponse;
 import com.suman.foodmandu.serverresponse.SignupResponse;
 
+import okhttp3.MultipartBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
 
 public interface Userapi {
 
     @POST("users/signup")
     Call<SignupResponse> registerUser(@Body User user);
+
+    @Multipart
+    @POST("upload")
+    Call<ImageResponse> uploadImage(@Part MultipartBody.Part img);
+
+
 }
